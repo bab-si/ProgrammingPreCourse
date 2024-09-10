148b1a8e6348c8a1d14b4c74b63f821352a6c456
Barbara Hoffmann <barbara.hoffmann@uni-bayreuth.de>
2024-09-10 20:11:31 +0200

    add ArrayExample example

---
+++ src/ArrayExample.java
@@ -0,0 +1,22 @@
+public class ArrayExample {
+    public static void main(String[] args) {
+        // Deklaration und Initialisierung eines Arrays von int
+        String[] myFavouriteColors = new String[5];
+
+        // Zuweisung von Werten zu den Array-Elementen
+        myFavouriteColors[0] = "Pink";
+        myFavouriteColors[1] = "Green";
+        myFavouriteColors[2] = "Blue";
+
+        // Zugriff auf ein Array-Element und Ausgabe
+        System.out.println("Erstes Element: " + myFavouriteColors[0]);
+
+        // Länge des Arrays
+        System.out.println("Länge des Arrays: " + myFavouriteColors.length);
+
+        // Schleife durch das Array
+        for (int i = 0; i < myFavouriteColors.length; i++) {
+            System.out.println("Element bei Index " + i + ": " + myFavouriteColors[i]);
+        }
+    }
+}
