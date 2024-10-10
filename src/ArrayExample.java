3a5cbaaa705a1fb625e03b9860c50b3ef58cc664
Barbara Hoffmann <barbara.hoffmann@uni-bayreuth.de>
2024-10-10 22:03:21 +0200

    add Literalinitialisierung in ArrayExample

--- src/ArrayExample.java
+++ src/ArrayExample.java
@@ -1,12 +1,15 @@
 public class ArrayExample {
     public static void main(String[] args) {
         // Deklaration und Initialisierung eines Arrays von int
-        String[] myFavouriteColors = new String[5];
+        String[] myFavouriteColors = new String[3];
 
         // Zuweisung von Werten zu den Array-Elementen
         myFavouriteColors[0] = "Pink";
         myFavouriteColors[1] = "Green";
         myFavouriteColors[2] = "Blue";
+
+        //Literalinitialisierung ohne new Operator
+        String[] myFavouriteColors2 = {"Pink", "Green", "Blue"};
 
         // Zugriff auf ein Array-Element und Ausgabe
         System.out.println("Erstes Element: " + myFavouriteColors[0]);
